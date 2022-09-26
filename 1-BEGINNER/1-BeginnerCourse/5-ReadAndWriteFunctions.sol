191bb732cb3e79f8e8bbaf62d09ec495e6279e50
Chidozie Ezeanekwe <chidozieezeanekwe@gmail.com>
2022-09-26 08:15:05 +0100

    Update 5-ReadAndWriteFunctions.sol

--- 1-BEGINNER/1-BeginnerCourse/5-ReadAndWriteFunctions.sol
+++ 1-BEGINNER/1-BeginnerCourse/5-ReadAndWriteFunctions.sol
@@ -1,23 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-contract Variables {
-    // State variables are stored on the blockchain.
-    string public text = "Hello";
-    uint public num = 123;
-    uint public blockNumber;
+contract SimpleStorage {
+    // State variable to store a number
+    uint public num;
 
+    // You need to send a transaction to write to a state variable.
+    function set(uint _num) public {
+        num = _num;
+    }
 
-    function doSomething() public {
-        // Local variables are not saved to the blockchain.
-        uint i = 456;
-
-        // Here are some global variables (also called special variables)
-        // used to retrieve information about the blockchain, particular addresses, contracts, and transactions.
-        uint timestamp = block.timestamp; // Current block timestamp
-        address sender = msg.sender; // address of the caller
-        blockNumber = block.number; // current block number
+    // You can read from a state variable without sending a transaction.
+    function get() public view returns (uint) {
+        return num;
     }
 }
-// complete list of global variables
-// https://docs.soliditylang.org/en/latest/cheatsheet.html?highlight=Variables#global-variables
